6db18d192ef1c8c509b39a0b12700f7c8f637f75
Roman Skrypnik <r.skrypnik@yual.io>
2024-03-27 20:36:40 +0200

    init

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div ref="containerRef" />
+</template>
+<script setup>
+import lottie from 'lottie-web/build/player/lottie_light';
+
+const containerRef = shallowRef();
+
+onMounted(async () => {
+  lottie.useWebWorker(true);
+
+  const animationData = {
+    container: containerRef.value,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets1.lottiefiles.com/datafiles/HN7OcWNnoqje6iXIiZdWzKxvLIbfeCGTmvXmEm1h/data.json',
+  };
+
+  lottie.loadAnimation(animationData);
+});
+</script>
